7c43fc9d12e4dbe5de54b33ded2b699e3f5f29b1
CharliSIO <c.jones89623@gmail.com>
2025-05-21 14:55:28 +1200

    Input tracking

--- BestDigitalGameForImpactGame/Assets/Scripts/InputTracking.cs
+++ BestDigitalGameForImpactGame/Assets/Scripts/InputTracking.cs
@@ -1,12 +1,40 @@
 using UnityEngine;
+using System.IO;
 
 public class InputTracking : MonoBehaviour
 {
+    private string logFilePath;
+
+    private void Start()
+    {
+        logFilePath = Path.GetFullPath(Path.Combine(Application.dataPath, "..")); // gets project path, one up 
+        logFilePath = Path.Combine(logFilePath, "actionLog.txt");
+        File.WriteAllText(logFilePath, "Input Log\n"); // overwrites previous text if it exists
+        Debug.Log("Logging to " + logFilePath);
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // detect key press
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                LogKey(key.ToString());
+            }
+        }
+
+        // detect mouse
+        if (Input.GetMouseButtonDown(0)) LogKey("LeftClick");
+        if (Input.GetMouseButtonDown(1)) LogKey("RightClick");
+        if (Input.GetMouseButtonDown(2)) LogKey("MiddleClick");
     }
-    
+
+    private void LogKey(string button)
+    {
+        string logEntry = Time.time.ToString("F2") + "s: " + button; // gets time key is pressed, to 5 decimal places
+        File.AppendAllText(logFilePath, logEntry + "\n"); // appends to file
+        Debug.Log(logEntry);
+    }
 }
